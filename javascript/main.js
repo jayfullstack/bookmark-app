bb14eb175a0c313099ec00e2f6168189b23c2804
jayfullstack <jayfullstack@yahoo.com>
2021-09-20 21:19:13 -0400

    implemented local storage

--- javascript/main.js
+++ javascript/main.js
@@ -6,6 +6,9 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
+
 // Show Modal, focus on Input
 
 function showModal(){
@@ -19,18 +22,64 @@ modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
 
+// Validate Form
+function validate(nameValue, urlValue){
+	const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+	const regex = new RegExp(expression);
+	if (!nameValue || !urlValue) {
+		alert('Please submit values for both fields.');
+		return false;
+	}
+	if(!urlValue.match(regex)){
+		alert('Please Provide a valid web address');
+		return false;
+	}
+	// Valid
+	return true;
+}
+
+// Fetch Bookmarks
+function fetchBookmarks(){
+	// Get bookmarks from localStorage if available
+	if (localStorage.getItem('bookmarks')) {
+		bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+	} else{
+		// Create bookmarks array in localStorage
+		bookmarks = [
+		{
+			name: 'Jannetti Design',
+			url: 'http://www.jamesjannetti.com'
+		}]	
+	}
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+}
+
 // Handle Data from Form
 function storeBookmark(e){
 	e.preventDefault();
 	const nameValue = websiteNameEl.value;
 	let urlValue = websiteUrlEl.value;
+	if (!urlValue.includes('https://') && !urlValue.includes('http://')){
+		urlValue = `https://${urlValue}`;
+	}
+	if (!validate(nameValue,urlValue)) {
+		return false;
+	}
+	const bookmark = {
+		name: nameValue,
+		url: urlValue,
+	};
+	// Adds to bookmarks array
+	bookmarks.push(bookmark);
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	fetchBookmarks();
+	// Resets the form
+	bookmarkForm.reset();
+	websiteNameEl.focus();
 }
 
 // Event Listener
 bookmarkForm.addEventListener('submit', storeBookmark);
 
-
-
-// if (!urlValue.includes('https://') && !urlValue.includes('http://')){
-// 	urlValue = `https://${urlValue}`;
-// }
+// On Load, Fetch Bookmarks
+fetchBookmarks();
