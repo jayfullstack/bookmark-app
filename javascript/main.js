f2273ea44944d0e57936a465574bac4b6b6cd18a
jayfullstack <jayfullstack@yahoo.com>
2021-09-20 21:48:15 -0400

    You can now delete bookmarks

--- javascript/main.js
+++ javascript/main.js
@@ -38,6 +38,41 @@ function validate(nameValue, urlValue){
 	return true;
 }
 
+// Build Bookmarks DOM
+function buildBookmarks(){
+	// Remove all bookmark elements
+	bookmarksContainer.textContent = '';
+	// Build items
+	// Runs a function on each element in the array
+	bookmarks.forEach((bookmark) =>{
+		const { name, url} = bookmark;
+		// Item
+		const item = document.createElement('div');
+		item.classList.add('item');
+		// Close Icon
+		const closeIcon = document.createElement('i');
+		closeIcon.classList.add('fas', 'fa-times', 'close-icon');
+		closeIcon.setAttribute('title', 'Delete Bookmark');
+		closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+		// Favicon / Link Container
+		const linkInfo = document.createElement('div');
+		linkInfo.classList.add('name');
+		// Favicon
+		const favicon = document.createElement('img');
+		favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+		favicon.setAttribute('alt', 'Favicon');
+		// Link
+		const link = document.createElement('a');
+		link.setAttribute('href', `${url}`);
+		link.setAttribute('target', '_blank');
+		link.textContent = name;
+		// Append to bookmarks container
+		linkInfo.append(favicon, link);
+		item.append(closeIcon, linkInfo);
+		bookmarksContainer.appendChild(item);
+	});
+}
+
 // Fetch Bookmarks
 function fetchBookmarks(){
 	// Get bookmarks from localStorage if available
@@ -49,9 +84,22 @@ function fetchBookmarks(){
 		{
 			name: 'Jannetti Design',
 			url: 'http://www.jamesjannetti.com'
-		}]	
+		}];
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 	}
+	buildBookmarks();
+}
+
+// Delete Bookmarks
+function deleteBookmark(url){
+	bookmarks.forEach((bookmark,i) => {
+		if(bookmark.url === url){
+		  bookmarks.splice(i, 1);
+		}
+	});
+	// UPDATE BOOKMARKS ARRAY IN LOCALSTORAGE
 	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	fetchBookmarks();
 }
 
 // Handle Data from Form
